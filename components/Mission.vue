d65eeb1ef2ca662f71baccafa9458de4994e9f53
ElianKas <e.kasteleiner@outlook.de>
2024-03-27 15:37:21 +0100

    Update Mission.vue

--- components/Mission.vue
+++ components/Mission.vue
@@ -5,13 +5,11 @@ onMounted(() => {
   $gsap.to('#scrollText', {
     scrollTrigger: {
       trigger: '#mission',
-      start: 'top 80%',
-      end: 'bottom 20%',
+      start: 'top 75%',
+      end: 'bottom 25%',
       scrub: 1,
     },
-    text: 'We drive the ecological, economic, and societal transformation of organizations and territories. We offer
-        comprehensive expertise to facilitate the transition to sustainable practices and lay the foundation for lasting
-        development.',
+    text: 'We drive the <br> ecological, economic, <br> and societal <br> transformation of <br> organizations and <br> territories. We offer <br> comprehensive <br> expertise to facilitate <br> the transition to <br> sustainable practices <br> and lay the foundation <br> for lasting <br> development.',
     ease: 'none',
   });
 });
@@ -19,8 +17,8 @@ onMounted(() => {
 <template>
   <section id="mission" class="h-auto flex flex-col justify-center px-[2rem] py-[8rem]">
     <h2 class="text-primary text-[1.5rem]">Mission: CNEDD</h2>
-    <div class="relative">
-      <p class="text-[2rem] text-lightGreen">
+    <div class="relative text-[2rem]">
+      <p class="text-lightGreen">
         We drive the ecological, economic, and societal transformation of organizations and territories. We offer
         comprehensive expertise to facilitate the transition to sustainable practices and lay the foundation for lasting
         development.
