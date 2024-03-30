551eb94ca3bd28af1e4a20dde514ee84849c83f3
ElianKas <e.kasteleiner@outlook.de>
2024-03-30 12:18:49 +0100

    Update Mission.vue

--- components/Mission.vue
+++ components/Mission.vue
@@ -19,8 +19,18 @@ onMounted(() => {
     <h2 class="text-primary text-[1.5rem]">Mission: CNEDD</h2>
     <div class="relative text-[4vh]">
       <p class="text-lightGreen">
-        We drive the ecological, economic, and societal transformation of organizations and territories. We offer
-        comprehensive expertise to facilitate the transition to sustainable practices and lay the foundation for lasting
+        We drive the <br />
+        ecological, economic, <br />
+        and societal <br />
+        transformation of <br />
+        organizations and <br />
+        territories. We offer <br />
+        comprehensive <br />
+        expertise to facilitate <br />
+        the transition to <br />
+        sustainable practices <br />
+        and lay the foundation <br />
+        for lasting <br />
         development.
       </p>
       <p id="scrollText" class="absolute top-0 text-darkGreen"></p>
