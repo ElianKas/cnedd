21dbdbbadd6ae7bcc0df647e4256ae5a6d8160aa
ElianKas <e.kasteleiner@outlook.de>
2024-04-01 20:23:45 +0200

    Update Contact.vue

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="px-[1rem]">
+  <section class="px-[1rem] text-white">
     <div class="w-full aspect-[4/5] bg-primary rounded-[8px] flex flex-col justify-center gap-[4rem] px-[2rem]">
       <h2>Become a part of the mission.</h2>
       <div>
