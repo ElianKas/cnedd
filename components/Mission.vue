2d4d464909c52cf8b7ef5833621c52f04646f026
ElianKas <e.kasteleiner@outlook.de>
2024-04-03 13:40:29 +0200

    Update Mission.vue

--- components/Mission.vue
+++ components/Mission.vue
@@ -4,7 +4,7 @@ const { $gsap } = useNuxtApp();
 let mm = $gsap.matchMedia();
 
 onMounted(() => {
-  mm.add('(min-width: 0px)', () => {
+  mm.add('(max-width: 767px)', () => {
     $gsap.to('#scrollText', {
       scrollTrigger: {
         trigger: '#mission',
@@ -24,19 +24,7 @@ onMounted(() => {
         end: 'bottom 25%',
         scrub: 1,
       },
-      text: 'We drive the ecological, economic, <br> and societal transformation of <br> organizations and territories. We <br> offer comprehensive expertise to <br> facilitate the transition to <br> sustainable practices and lay the <br> foundation for lasting <br> development.',
-      ease: 'none',
-    });
-  });
-  mm.add('(min-width: 1024px)', () => {
-    $gsap.to('#scrollText', {
-      scrollTrigger: {
-        trigger: '#mission',
-        start: 'top 75%',
-        end: 'bottom 25%',
-        scrub: 1,
-      },
-      text: 'We drive the ecological, <br> economic, and societal <br> transformation of organizations <br> and territories. We offer <br> comprehensive expertise to <br> facilitate the transition to <br> sustainable practices and lay the <br> foundation for lasting <br> development.',
+      text: 'We drive the ecological, economic, and societal transformation of organizations and territories. We offer comprehensive expertise to facilitate the transition to sustainable practices and lay the foundation for lasting development.',
       ease: 'none',
     });
   });
@@ -48,7 +36,7 @@ onMounted(() => {
     class="h-auto flex flex-col justify-center px-[2rem] lg:px-[5rem] max-w-[1280px] m-auto py-[8rem]"
   >
     <h2 class="text-primary text-[1.5rem]">Mission: CNEDD</h2>
-    <div class="relative text-[8.3vw] md:text-[6vw] xl:text-[4.7rem]">
+    <div class="relative text-[8.3vw] xl:text-[5vw]">
       <p class="text-lightGreen">
         We drive the ecological, economic, and societal transformation of organizations and territories. We offer
         comprehensive expertise to facilitate the transition to sustainable practices and lay the foundation for lasting
