61480a82d2f02c7aaa81161eb60b2052d1b621bb
ElianKas <e.kasteleiner@outlook.de>
2024-04-04 14:55:48 +0200

    Update Mission.vue

--- components/Mission.vue
+++ components/Mission.vue
@@ -4,39 +4,37 @@ const { $gsap } = useNuxtApp();
 let mm = $gsap.matchMedia();
 
 onMounted(() => {
-  mm.add('(max-width: 767px)', () => {
+  mm.add('(max-width: 1023px)', () => {
     $gsap.to('#scrollText', {
       scrollTrigger: {
         trigger: '#mission',
         start: 'top 75%',
-        end: 'bottom 25%',
+        end: 'bottom 75%',
         scrub: 1,
       },
-      text: 'We drive the <br> ecological, economic, <br> and societal <br> transformation of <br> organizations and <br> territories. We offer <br> comprehensive <br> expertise to facilitate <br> the transition to <br> sustainable practices <br> and lay the foundation <br> for lasting <br> development.',
+      text: 'We drive the <br> ecological, economic, <br> and societal <br> transformation of <br> organizations and <br> territories. We offer <br> comprehensive <br> expertise to facilitate <br> the transition to <br> sustainable practices <br> and lay the <br> foundation for lasting <br> development.',
       ease: 'none',
     });
   });
-  mm.add('(min-width: 768px)', () => {
+  mm.add('(min-width: 1024px)', () => {
     $gsap.to('#scrollText', {
       scrollTrigger: {
         trigger: '#mission',
         start: 'top 75%',
-        end: 'bottom 25%',
+        end: 'bottom 75%',
         scrub: 1,
       },
-      text: 'We drive the ecological, economic, and societal transformation of organizations and territories. We offer comprehensive expertise to facilitate the transition to sustainable practices and lay the foundation for lasting development.',
+      text: 'We drive the ecological, economic, and <br> societal transformation of organizations and <br> territories. We offer comprehensive expertise <br> to facilitate the transition to sustainable <br> practices and lay the foundation for lasting <br> development.',
       ease: 'none',
     });
   });
 });
 </script>
 <template>
-  <section
-    id="mission"
-    class="h-auto flex flex-col justify-center px-[2rem] lg:px-[5rem] max-w-[1280px] m-auto py-[8rem]"
-  >
+  <section id="mission" class="h-auto w-[80vw] flex flex-col justify-center m-auto py-[8rem] lg:w-[60vw]">
     <h2 class="text-primary text-[1.5rem]">Mission: CNEDD</h2>
-    <div class="relative text-[8.3vw] xl:text-[5vw]">
+    <br />
+    <div class="relative text-[8.3vw] lg:text-[3vw]">
       <p class="text-lightGreen">
         We drive the ecological, economic, and societal transformation of organizations and territories. We offer
         comprehensive expertise to facilitate the transition to sustainable practices and lay the foundation for lasting
