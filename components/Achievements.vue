e47136628097e92afaa07803ab4665c3408c9c76
ElianKas <e.kasteleiner@outlook.de>
2024-04-06 15:59:55 +0200

    g

--- components/Achievements.vue
+++ components/Achievements.vue
@@ -3,6 +3,8 @@ const achievements = [
   { value1: 'value1', value2: 'value2' },
   { value1: 'value3', value2: 'value4' },
   { value1: 'value5', value2: 'value6' },
+  { value1: 'value7', value2: 'value8' },
+  { value1: 'value9', value2: 'value0' },
 ];
 const settings = ref({
   itemsToShow: 1.1,
@@ -11,10 +13,21 @@ const settings = ref({
   mouseDrag: true,
   touchDrag: true,
 });
+const breakpoints = {
+  640: {
+    itemsToShow: 2.5,
+  },
+  768: {
+    itemsToShow: 3.5,
+  },
+  1024: {
+    itemsToShow: 4,
+  },
+};
 </script>
 <template>
   <section class="text-center">
-    <Carousel :transition="500" v-bind="settings">
+    <Carousel :transition="500" v-bind="settings" :breakpoints="breakpoints">
       <Slide v-for="achievement in achievements" :key="achievement">
         <div class="bg-primary mx-[0.5rem] h-[6rem] w-full rounded-[8px] text-white flex flex-col justify-center">
           <p>{{ achievement.value1 }}</p>
--- components/News.vue
+++ components/News.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="pt-[8rem]">
-    <p class="text-[1.5rem] px-[2rem]">
+  <section class="max-lg:pt-[8rem] py-[8rem] lg:flex lg:items-center lg:gap-[2rem]">
+    <p class="text-[1.5rem] px-[2rem] lg:w-[50%]">
       We are about to set up our own plastic recycling station to open up new possibilities in yaounde, so people get
       aware, get creative and create their own future with opening up job opportunities in plastic recycling product
       production.
     </p>
-    <img src="" alt="" class="rounded-[8px] w-full aspect-[4/5] px-[1rem]" />
+    <img src="" alt="" class="rounded-[8px] w-full aspect-[4/5] lg:aspect-video px-[1rem] lg:w-[50%]" />
   </section>
 </template>
