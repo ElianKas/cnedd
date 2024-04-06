35b56225f5f982f380ffb0a584d59ef5c874b421
ElianKas <e.kasteleiner@outlook.de>
2024-04-06 16:06:56 +0200

    d

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,6 +1,8 @@
 <template>
-  <section class="px-[1rem] text-white">
-    <div class="w-full aspect-[4/5] bg-primary rounded-[8px] flex flex-col justify-center gap-[4rem] px-[2rem]">
+  <section class="px-[1rem] text-white lg:max-w-[60vw] m-auto">
+    <div
+      class="w-full aspect-[4/5] lg:aspect-[120/34] bg-primary rounded-[8px] flex flex-col justify-center gap-[4rem] px-[2rem]"
+    >
       <h2>Become a part of the mission.</h2>
       <div>
         <p>Get in touch with us!</p>
--- components/Socials.vue
+++ components/Socials.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="px-[3rem] py-[8rem]">
-    <div class="mb-[3rem]">
+  <section class="px-[3rem] py-[8rem] lg:max-w-[40vw] m-auto lg:flex lg:gap-[2rem]">
+    <div class="mb-[3rem] lg:w-[50%]">
       <h2>Stay up date</h2>
       <br />
       <p>Follow us on Instagram for a closer look into our work.</p>
     </div>
-    <p>@cnedd.cmr</p>
+    <p class="lg:w-[50%]">@cnedd.cmr</p>
   </section>
 </template>
