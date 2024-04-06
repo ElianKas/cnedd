a8e41d1da498de55a74c1d3e5a7d68446b96a27a
ElianKas <e.kasteleiner@outlook.de>
2024-04-06 16:14:41 +0200

    Update Partners.vue

--- components/Partners.vue
+++ components/Partners.vue
@@ -2,6 +2,7 @@
 const partners = [
   { image: '', name: 'Titel 1' },
   { image: '', name: 'Titel 2' },
+  { image: '', name: 'Titel 3' },
 ];
 const settings = ref({
   itemsToShow: 1.8,
@@ -9,13 +10,23 @@ const settings = ref({
   wrapAround: true,
   mouseDrag: true,
   touchDrag: true,
+  modelValue: 1,
 });
+const breakpoints = {
+  640: {
+    itemsToShow: 2.5,
+  },
+  768: {
+    itemsToShow: 4,
+    wrapAround: false,
+  },
+};
 const carousel2 = ref(null);
 </script>
 <template>
-  <section class="text-center py-[8rem]">
+  <section class="text-center">
     <h2 class="mb-[4rem]">Our Partners for Change</h2>
-    <Carousel ref="carousel2" :transition="500" v-bind="settings">
+    <Carousel ref="carousel2" :transition="500" v-bind="settings" :breakpoints="breakpoints">
       <Slide v-for="partner in partners" :key="partner">
         <div class="mx-[0.5rem] h-auto w-full flex flex-col justify-between items-center gap-[2rem] px-[1.5rem]">
           <img :src="`${partner.image}`" alt="" class="rounded-full w-[12rem] aspect-square border" />
