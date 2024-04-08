a369b22f3af787c4c19081211706a136b424c501
ElianKas <e.kasteleiner@outlook.de>
2024-04-08 15:23:35 +0200

    Update Sub.vue

--- components/Sub.vue
+++ components/Sub.vue
@@ -3,12 +3,28 @@
     class="h-[28rem] lg:h-[14rem] bg-primary flex flex-col lg:flex-row justify-center items-center gap-[5rem] text-white font-bold"
   >
     <div class="text-center">
-      <p>img</p>
+      <div class="m-auto w-[90px] aspect-square rounded-full border-2 flex justify-center items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M12 12.5q1.258 0 2.129-.871T15 9.5t-.871-2.129T12 6.5t-2.129.871T9 9.5t.871 2.129T12 12.5m0 8.462q-3.227-.964-5.113-3.712T5 11.1V5.692l7-2.615l7 2.615V11.1q0 3.402-1.887 6.15q-1.886 2.748-5.113 3.712m0-1.062q1.34-.437 2.43-1.305t1.882-2.028q-.999-.511-2.084-.79Q13.142 15.5 12 15.5t-2.228.278t-2.084.79q.793 1.159 1.882 2.027q1.09.868 2.43 1.305"
+          />
+        </svg>
+      </div>
+      <br />
       <h2>promotion of sustainable development</h2>
     </div>
     <div class="text-center">
-      <p>img</p>
-      <h2>promotion of sustainable development</h2>
+      <div class="m-auto w-[90px] aspect-square rounded-full border-2 flex justify-center items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M12 8H4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h1v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h3l5 4V4zm9.5 4c0 1.71-.96 3.26-2.5 4V8c1.53.75 2.5 2.3 2.5 4"
+          />
+        </svg>
+      </div>
+      <br />
+      <h2>environmental protection in Cameroon</h2>
     </div>
   </section>
 </template>
