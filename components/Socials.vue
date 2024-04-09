96efffd8c76e35c022b2e92139937a8746bb26c6
ElianKas <e.kasteleiner@outlook.de>
2024-04-09 18:50:43 +0200

    e

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,10 +1,39 @@
 <template>
-  <section class="px-[3rem] py-[8rem] lg:max-w-[40vw] m-auto lg:flex lg:gap-[2rem]">
+  <section class="px-[3rem] py-[8rem] lg:max-w-[40vw] m-auto lg:flex">
     <div class="mb-[3rem] lg:w-[50%]">
       <h2>Stay up date</h2>
       <br />
       <p>Follow us on Instagram for a closer look into our work.</p>
     </div>
-    <p class="lg:w-[50%]">@cnedd.cmr</p>
+    <div class="lg:w-[50%] flex flex-col items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        width="60"
+        height="60"
+        viewBox="0 0 85.557 85.574"
+      >
+        <defs>
+          <linearGradient id="a" y1="1" x2="1" gradientUnits="objectBoundingBox">
+            <stop offset="0" stop-color="#fd5" />
+            <stop offset="0.5" stop-color="#ff543e" />
+            <stop offset="1" stop-color="#c837ab" />
+          </linearGradient>
+        </defs>
+        <g transform="translate(0.07 0.039)">
+          <path
+            d="M5.511,5.9C-1.193,12.866.179,20.264.179,42.736c0,18.662-3.256,37.37,13.785,41.774,5.321,1.369,52.47,1.369,57.784-.007,7.1-1.831,12.868-7.586,13.657-17.62.11-1.4.11-46.868,0-48.3C84.563,7.9,77.983,1.737,69.313.489,67.326.2,66.928.116,56.733.1,20.572.116,12.645-1.494,5.511,5.9Z"
+            transform="translate(0 0)"
+            fill="url(#a)"
+          />
+          <path
+            d="M34.645,3.161C21.738,3.161,9.482,2.013,4.8,14.027,2.866,18.99,3.147,25.434,3.147,34.662c0,8.1-.259,15.708,1.653,20.631,4.671,12.022,17.027,10.87,29.838,10.87,12.359,0,25.1,1.287,29.841-10.87,1.937-5.012,1.653-11.361,1.653-20.631,0-12.306.679-20.251-5.289-26.216C54.8,2.4,46.628,3.161,34.631,3.161ZM31.823,8.838C58.745,8.795,62.172,5.8,60.281,47.381c-.672,14.706-11.869,13.092-25.633,13.092-25.1,0-25.817-.718-25.817-25.824,0-25.4,1.991-25.8,22.991-25.817Zm19.636,5.229a3.779,3.779,0,1,0,3.779,3.779A3.779,3.779,0,0,0,51.458,14.066ZM34.645,18.485a16.175,16.175,0,1,0,16.17,16.177,16.176,16.176,0,0,0-16.17-16.177Zm0,5.677c13.881,0,13.9,21,0,21C20.768,45.163,20.746,24.162,34.645,24.162Z"
+            transform="translate(8.183 8.095)"
+            fill="#fff"
+          />
+        </g>
+      </svg>
+      <p class="mt-[1rem] text-[#FF553F]">@cnedd.cmr</p>
+    </div>
   </section>
 </template>
