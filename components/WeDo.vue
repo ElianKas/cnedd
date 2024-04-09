cbfa343f61a323655d2d813c5df82d6544ca9a6e
ElianKas <e.kasteleiner@outlook.de>
2024-04-09 18:37:29 +0200

    Update WeDo.vue

--- components/WeDo.vue
+++ components/WeDo.vue
@@ -1,55 +1,38 @@
-<script setup>
-const skills = [
-  { title: 'Titel 1', subtitle: 'sub 1' },
-  { title: 'Titel 2', subtitle: 'sub 2' },
-];
-const settings = ref({
-  itemsToShow: 1.2,
-  snapAlign: 'center',
-  wrapAround: true,
-  mouseDrag: false,
-  touchDrag: false,
-});
-const carousel1 = ref(null);
-</script>
 <template>
-  <section class="text-center">
-    <Carousel ref="carousel1" :transition="500" v-bind="settings">
-      <Slide v-for="skill in skills" :key="skill">
-        <div
-          class="bg-primary mx-[0.5rem] h-[30rem] w-full rounded-[8px] flex flex-col justify-end px-[2rem] py-[1rem]"
-        >
-          <p>{{ skill.title }}</p>
-          <p>{{ skill.subtitle }}</p>
-        </div>
-      </Slide>
-    </Carousel>
-    <div class="flex justify-center mt-[2rem]">
-      <div
-        class="flex justify-between items-center w-[10rem] h-[4rem] bg-lightGreen rounded-full p-[0.4rem] text-darkGreen"
-      >
-        <div
-          @click="carousel1.prev()"
-          class="border-[1px] rounded-full h-full aspect-square bg-white flex justify-center items-center cursor-pointer grow-0"
-        >
-          <svg class="ml-[-0.3rem]" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="m9.55 12l7.35 7.35q.375.375.363.875t-.388.875q-.375.375-.875.375t-.875-.375l-7.7-7.675q-.3-.3-.45-.675t-.15-.75q0-.375.15-.75t.45-.675l7.7-7.7q.375-.375.888-.363t.887.388q.375.375.375.875t-.375.875z"
-            />
-          </svg>
-        </div>
-        <div
-          @click="carousel1.next()"
-          class="border-[1px] rounded-full h-full aspect-square bg-white flex justify-center items-center cursor-pointer grow-0"
-        >
-          <svg class="mr-[-0.3rem]" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="m14.475 12l-7.35-7.35q-.375-.375-.363-.888t.388-.887q.375-.375.888-.375t.887.375l7.675 7.7q.3.3.45.675t.15.75q0 .375-.15.75t-.45.675l-7.7 7.7q-.375.375-.875.363T7.15 21.1q-.375-.375-.375-.888t.375-.887z"
-            />
-          </svg>
-        </div>
+  <section class="w-[80vw] lg:w-[60vw] m-auto">
+    <h2>WHAT WE DO</h2>
+    <br />
+    <div class="grid grid-cols-1 auto-rows-auto gap-[3rem] sm:grid-cols-2 xl:grid-cols-3">
+      <div>
+        <h3>Greenskills Bootcamp</h3>
+        <br />
+        <p>Introductory program to green entrepreneurship and the circular economy</p>
+      </div>
+      <div>
+        <h3>One Citizen One Tree</h3>
+        <br />
+        <p>Engage global citizens in favor of reforestation in Cameroon</p>
+      </div>
+      <div>
+        <h3>Sustainable Developement Ed</h3>
+        <br />
+        <p>
+          Promote the objectives of sustainable development in the Cameroonian context for the involvement of all
+          stakeholders and the achievement of these objectives
+        </p>
+      </div>
+      <div>
+        <h3>Clean Cameroon Cities</h3>
+        <br />
+        <p>
+          Mobilize local communities for sanitation actions and pre-collection of plastic waste for recycling with our
+          partners
+        </p>
+      </div>
+      <div>
+        <h3>Soley Recycling Project</h3>
+        <br />
+        <p>Recycle plastic waste with our partners into useful, innovative and sustainable products</p>
       </div>
     </div>
   </section>
