9a84fa98c7a758cf6d4499c296c6edfd60fd74a9
ElianKas <e.kasteleiner@outlook.de>
2024-04-09 13:22:30 +0200

    Update Landing.vue

--- components/Landing.vue
+++ components/Landing.vue
@@ -4,9 +4,10 @@
       src="/images/Landing.jpeg"
       format="avif"
       class="absolute top-0 left-0 w-full h-full z-[-99] object-cover"
+      quality="30"
+      placeholder
     />
-    <div class="text-center leading-none">
-      <p>Cameroon</p>
+    <div class="text-center text-white leading-none">
       <h1 class="text-[5.5rem]">CNEDD</h1>
       <p class="text-[2rem]">Stronger Together</p>
     </div>
